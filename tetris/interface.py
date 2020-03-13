b0af25ff437e5f560d2e27bede581c758b5a4806
Andrea Gasparini <iogaspa@gmail.com>
2020-03-13 22:36:44 +0100

    Adds simple piece dropping
    * interface draws piece squares
    * each step piece drop down by one position
    * board check collision, merge the piece and reinstance another one

--- tetris/interface.py
+++ tetris/interface.py
@@ -10,12 +10,14 @@ from tetris.models import Board
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
+green = (0, 255, 0)
 
 
 class Tetris(object):
     def __init__(self):
         self.board = Board()
         self.screen = None
+        self.board.add_piece()
 
     def init(self):
         pygame.init()
@@ -25,6 +27,7 @@ class Tetris(object):
     def draw_board(self):
         (block_width, block_height) = block_size = (screen_width / self.board.cols, screen_height / self.board.rows)
         block_surface = pygame.Surface(block_size)
+
         for row_number, row in enumerate(self.board.landed):
             for col_number, landed in enumerate(row):
                 # print(f"in {row_number}-{col_number} landed is {landed}")
@@ -36,11 +39,25 @@ class Tetris(object):
                     block_surface.fill(black)
                 self.screen.blit(block_surface, block_rect)
 
+        for piece in self.board.pieces:
+            for row_number, piece_rows in enumerate(piece.shape):
+                for column_number, piece_value in enumerate(piece_rows):
+                    row = piece.row + row_number
+                    column = piece.column + column_number
+                    position = (block_width * column, block_height * row)
+                    block_rect = pygame.Rect(position, block_size)
+                    if piece_value:
+                        block_surface.fill(green)
+                        self.screen.blit(block_surface, block_rect)
+
     def run_tetris(self):
         while 1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.board.next()
 
             self.screen.fill(black)
             # draw the board:
--- tetris/models.py
+++ tetris/models.py
@@ -3,8 +3,20 @@
 
 class Piece(object):
     def __init__(self):
-        self.shape = []
+        self.shape = self._generate_shape()
         self.position = (0, 0)
+
+    @staticmethod
+    def _generate_shape():
+        return [[1, 1], [1, 1]]
+
+    @property
+    def row(self):
+        return self.position[0]
+
+    @property
+    def column(self):
+        return self.position[1]
 
 
 class Board(object):
@@ -13,20 +25,50 @@ class Board(object):
     def __init__(self):
         self.landed = [[0] * Board.cols] * Board.rows
         # Test board with some landed block here and there...
-        # self.landed = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-        #                [0, 0, 0, 1, 1, 0, 0, 0, 0, 0]]
+        self.landed = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 1, 0, 0, 0, 0, 0]]
         self.pieces = []
+
+    def add_piece(self):
+        piece = Piece()
+        piece.position = (0, Board.cols // 2)
+        self.pieces.append(piece)
+
+    def next(self):
+        for piece in self.pieces:
+            new_position = (piece.position[0] + 1, piece.position[1])
+            if not self._check_collision(new_position, piece.shape):
+                piece.position = new_position
+            else:
+                self._update_landed(piece.position, piece.shape)
+                self._remove_piece(piece)
+                self.add_piece()
+
+    def _check_collision(self, position, shape):
+        for row, shape_row in enumerate(shape):
+            for column, shape_value in enumerate(shape_row):
+                if shape_value and self.landed[position[0] + row][position[1] + column]:
+                    return True
+        return False
+
+    def _update_landed(self, position, shape):
+        for row, shape_row in enumerate(shape):
+            for column, shape_value in enumerate(shape_row):
+                self.landed[position[0] + row][position[1] + column] = shape_value
+
+    def _remove_piece(self, piece):
+        self.pieces.remove(piece)
